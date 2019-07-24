4e000fc88f4f49a794d02bc45039afa20b4a3fd0
Ghaith <ghaith_bhs@yahoo.fr>
2019-07-24 14:20:23 +0100

    ready for test

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft import MycroftSkill, intent_file_handler
 
 
@@ -5,11 +7,22 @@ class FocusDefinition(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('definition.focus.intent')
-    def handle_definition_focus(self, message):
+    @intent_handler(IntentBuilder("").require('definition.focus'))
+    def handle_hr_location(self):
         self.speak_dialog('definition.focus')
+
+    @intent_handler(IntentBuilder("").require('focus.departments'))
+    def handle_hr_location(self):
+        self.speak_dialog('definition.departments')
+
+    @intent_handler(IntentBuilder("").require('focus.field'))
+    def handle_hr_location(self):
+        self.speak_dialog('definition.field')
+
+        @intent_handler(IntentBuilder("").require('focus.history'))
+        def handle_hr_location(self):
+            self.speak_dialog('definition.history')
 
 
 def create_skill():
     return FocusDefinition()
-
